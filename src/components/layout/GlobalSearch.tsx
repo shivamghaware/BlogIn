73cd5d57f54f4404f9603343fed29a10b2ea371b
Shivam Ghaware <shivamghaware3@gmail.com>
2025-11-12 15:40:10 +0000

    ADD link from the Search result to original post / User

--- src/components/layout/GlobalSearch.tsx
+++ src/components/layout/GlobalSearch.tsx
@@ -15,14 +15,36 @@ import {
 } from '@/components/ui/command';
 import type { Post, User as UserType } from '@/lib/types';
 import { getPosts, getUsers } from '@/lib/data';
+import Link from 'next/link';
+import { Command as CommandPrimitive } from 'cmdk';
 
 type GlobalSearchProps = {
     open: boolean;
     onOpenChange: (open: boolean) => void;
 }
 
-export function GlobalSearch({ open, onOpenChange }: GlobalSearchProps) {
+const CommandItemLink = React.forwardRef<
+  React.ElementRef<typeof CommandPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof CommandPrimitive.Item> & { href: string }
+>(({ href, ...props }, ref) => {
   const router = useRouter();
+  return (
+    <Link href={href} passHref legacyBehavior>
+      <CommandPrimitive.Item
+        ref={ref}
+        {...props}
+        onSelect={(value) => {
+          props.onSelect?.(value);
+          router.push(href);
+        }}
+      />
+    </Link>
+  );
+});
+CommandItemLink.displayName = 'CommandItemLink';
+
+
+export function GlobalSearch({ open, onOpenChange }: GlobalSearchProps) {
   const [posts, setPosts] = React.useState<Post[]>([]);
   const [users, setUsers] = React.useState<UserType[]>([]);
 
@@ -40,18 +62,19 @@ export function GlobalSearch({ open, onOpenChange }: GlobalSearchProps) {
 
   React.useEffect(() => {
     async function fetchAllData() {
-      const allPosts = await getPosts();
-      const allUsers = await getUsers();
-      setPosts(allPosts);
-      setUsers(allUsers);
+      if (open) {
+        const allPosts = await getPosts();
+        const allUsers = await getUsers();
+        setPosts(allPosts);
+        setUsers(allUsers);
+      }
     }
     fetchAllData();
-  }, []);
+  }, [open]);
 
-  const runCommand = React.useCallback((command: () => unknown) => {
+  const handleSelect = () => {
     onOpenChange(false);
-    command();
-  }, [onOpenChange]);
+  }
 
   return (
     <CommandDialog open={open} onOpenChange={onOpenChange}>
@@ -60,30 +83,28 @@ export function GlobalSearch({ open, onOpenChange }: GlobalSearchProps) {
         <CommandEmpty>No results found.</CommandEmpty>
         <CommandGroup heading="Posts">
           {posts.map((post) => (
-            <CommandItem
+            <CommandItemLink
               key={post.slug}
+              href={`/posts/${post.slug}`}
               value={`post-${post.slug}-${post.title}`}
-              onSelect={() => {
-                runCommand(() => router.push(`/posts/${post.slug}`));
-              }}
+              onSelect={handleSelect}
             >
               <FileText className="mr-2 h-4 w-4" />
               <span>{post.title}</span>
-            </CommandItem>
+            </CommandItemLink>
           ))}
         </CommandGroup>
         <CommandGroup heading="Users">
           {users.map((user) => (
-            <CommandItem
+            <CommandItemLink
               key={user.id}
+              href={`/profile/${user.id}`}
               value={`user-${user.id}-${user.name}`}
-              onSelect={() => {
-                runCommand(() => router.push(`/profile/${user.id}`));
-              }}
+              onSelect={handleSelect}
             >
               <User className="mr-2 h-4 w-4" />
               <span>{user.name}</span>
-            </CommandItem>
+            </CommandItemLink>
           ))}
         </CommandGroup>
       </CommandList>
